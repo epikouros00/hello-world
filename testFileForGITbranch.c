d0dfa824639f453542fb3d766e741fa4e88c1f4c
epikouros00 <epikouros00@gmail.com>
2019-12-02 12:44:57 +0900

    Test git-branch fucntion ---------------

---
+++ testFileForGITbranch.c
@@ -0,0 +1,15 @@
+/* This file is for testing git-branch function and then check the push remote .git */
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+
+int main(void)
+{
+    printf("This is just dummy c file for checking git-branch function\n");
+
+    return 1;
+}
+
+
